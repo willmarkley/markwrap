a60865a6d6cb2dbd8f7e455c5f310e53d789841a
Will Markley <willmarkley@users.noreply.github.com>
2019-06-26 01:50:45 -0400

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="markwrap",
-    version="0.0.2",
+    version="0.0.3",
     author="Will Markley",
     author_email="willmarkley@protonmail.com",
     description="Python wrappers around common dependencies",
