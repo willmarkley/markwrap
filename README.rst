22d334fedc3578c302ba6114577e5cb6936404bf
William Markley <willjmarkley@gmail.com>
2019-06-26 01:29:46 -0400

    add automatic testing and deployment to pypi

--- README.rst
+++ README.rst
@@ -2,4 +2,7 @@
 markwrap
 ========
 
+.. image:: https://travis-ci.org/willmarkley/markwrap.svg?branch=master
+    :target: https://travis-ci.org/willmarkley/markwrap
+
 Python wrappers around common dependencies
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="markwrap",
-    version="0.0.1",
+    version="0.0.2",
     author="Will Markley",
     author_email="willmarkley@protonmail.com",
     description="Python wrappers around common dependencies",
