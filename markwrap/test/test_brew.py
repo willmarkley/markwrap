0cd9d9ee5cead91e8794a023fb3a35a238cbd258
William Markley <willjmarkley@gmail.com>
2019-06-28 22:32:40 -0400

    Make test logging cross-platform

--- markwrap/test/test_brew.py
+++ markwrap/test/test_brew.py
@@ -9,8 +9,8 @@ Please manually test and verify before committing changes.
 def test_brew_list(caplog):
 	## ARRANGE ##
 	caplog.set_level(logging.DEBUG)
-	EXPECTED_LOG_1 = "INFO     root:brew.py:14 Process started   :  /usr/local/bin/brew list -1"
-	EXPECTED_LOG_2 = "INFO     root:brew.py:19 Process terminated:  /usr/local/bin/brew list -1"
+	EXPECTED_LOG_1 = "[INFO] brew.list - Process started   :  /usr/local/bin/brew list -1"
+	EXPECTED_LOG_2 = "[INFO] brew.list - Process terminated:  /usr/local/bin/brew list -1"
 
 	## ACT ##
 	brew.list()
@@ -24,8 +24,8 @@ def test_brew_list(caplog):
 def test_brew_update(caplog):
 	## ARRANGE ##
 	caplog.set_level(logging.DEBUG)
-	EXPECTED_LOG_1 = "INFO     root:brew.py:22 Process started   :  /usr/local/bin/brew update"
-	EXPECTED_LOG_2 = "INFO     root:brew.py:27 Process terminated:  /usr/local/bin/brew update"
+	EXPECTED_LOG_1 = "[INFO] brew.update - Process started   :  /usr/local/bin/brew update"
+	EXPECTED_LOG_2 = "[INFO] brew.update - Process terminated:  /usr/local/bin/brew update"
 
 	## ACT ##
 	brew.update()
@@ -39,8 +39,8 @@ def test_brew_update(caplog):
 def test_brew_upgrade(caplog):
 	## ARRANGE ##
 	caplog.set_level(logging.DEBUG)
-	EXPECTED_LOG_1 = "INFO     root:brew.py:30 Process started   :  /usr/local/bin/brew upgrade"
-	EXPECTED_LOG_2 = "INFO     root:brew.py:35 Process terminated:  /usr/local/bin/brew upgrade"
+	EXPECTED_LOG_1 = "[INFO] brew.upgrade - Process started   :  /usr/local/bin/brew upgrade"
+	EXPECTED_LOG_2 = "[INFO] brew.upgrade - Process terminated:  /usr/local/bin/brew upgrade"
 
 	## ACT ##
 	brew.upgrade()
@@ -54,9 +54,9 @@ def test_brew_upgrade(caplog):
 def test_brew_validateInstall(caplog):
 	## ARRANGE ##
 	caplog.set_level(logging.DEBUG)
-	EXPECTED_LOG_1 = "INFO     root:brew.py:38 Validating Homebrew install at /usr/local/bin/brew"
-	EXPECTED_LOG_2 = "INFO     root:brew.py:43 Homebrew found at /usr/local/bin/brew"
-	EXPECTED_LOG_3 = "INFO     root:brew.py:48 Validated Homebrew install at /usr/local/bin/brew"
+	EXPECTED_LOG_1 = "[INFO] brew.validateInstall - Validating Homebrew install at /usr/local/bin/brew"
+	EXPECTED_LOG_2 = "[INFO] brew.validateInstall - Homebrew found at /usr/local/bin/brew"
+	EXPECTED_LOG_3 = "[INFO] brew.validateInstall - Validated Homebrew install at /usr/local/bin/brew"
 
 	## ACT ##
 	brew.validateInstall()
