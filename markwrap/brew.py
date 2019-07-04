dc6b848074798a8b61cde58635c83c37e85761b8
William Markley <willjmarkley@gmail.com>
2019-07-04 01:48:07 -0400

    Validate Homebrew is installed

--- markwrap/brew.py
+++ markwrap/brew.py
@@ -10,6 +10,19 @@ import subprocess
 
 BREW="/usr/local/bin/brew"
 
+logging.info("Validating Homebrew install at %s", BREW)
+validate_result = shutil.which(BREW)
+if (validate_result is None):
+	logging.error("Homebrew not found with shutil at %s", BREW)
+	raise RuntimeError()
+logging.info("Homebrew found at %s", BREW)
+validate_result = subprocess.call([BREW,"--version"], subprocess.PIPE, stderr=subprocess.STDOUT)
+if (validate_result != 0):
+	logging.error("Error running process (exit code %d):  %s %s", validate_result, BREW, "--version")
+	raise RuntimeError()
+logging.info("Validated Homebrew install at %s", BREW)
+
+
 def list():
 	logging.info("Process started   :  %s %s %s", BREW, "list", "-1")
 	result = subprocess.call([BREW,"list","-1"], subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -33,16 +46,3 @@ def upgrade():
 		logging.error("Error running process:  %s %s", BREW, "upgrade")
 		raise RuntimeError()
 	logging.info("Process terminated:  %s %s", BREW, "upgrade")
-
-def validateInstall():
-	logging.info("Validating Homebrew install at %s", BREW)
-	result = shutil.which(BREW)
-	if (result is None):
-		logging.error("Homebrew not found with shutil at %s", BREW)
-		raise RuntimeError()
-	logging.info("Homebrew found at %s", BREW)
-	result = subprocess.call([BREW,"--version"], subprocess.PIPE, stderr=subprocess.STDOUT)
-	if (result != 0):
-		logging.error("Error running process (exit code %d):  %s %s", result, BREW, "--version")
-		raise RuntimeError()
-	logging.info("Validated Homebrew install at %s", BREW)
