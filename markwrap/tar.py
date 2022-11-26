b955307ef74aa83bbadc572f13ed760e4a93944a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 09:37:51 +0000

    Adding tarfile member sanitization to extractall()

--- markwrap/tar.py
+++ markwrap/tar.py
@@ -40,5 +40,24 @@ def decompress(tarball, destinationPath):
 
 	logging.info("Decompressing %s into %s", tarball, destinationPath)
 	with tarfile.open(tarball, "r:gz") as tar:
-		tar.extractall(destinationPath)
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar, destinationPath)
 	logging.info("Decompressed %s into %s", tarball, destinationPath)
